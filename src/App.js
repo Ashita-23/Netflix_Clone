81bc074ccfafadf830cbab685e96cd0063fb44d2
Akash <akashbytes@gmail.com>
2024-03-17 21:09:41 +0530

    browser and search component rendered...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
+// import SignIn from './Components/Sign_in_components/Sign_in';
 import './App.css';
+import Browse from './Components/Browse_Components/Browse'
+import Navigation from './Components/Browse_Components/header';
+import DoSearchPage from "./Components/Browse_Components/doSearchPage"
+
+
+
 
 function App() {
-  return (
-    <div className="border border-red-600">
-helooo
-    </div>
-  );
+  return (<>
+  {/* <SignIn/> */}
+<Navigation></Navigation>
+  {/* <Browse></Browse> */}
+  <DoSearchPage></DoSearchPage>
+
+  </>  );
 }
 
 export default App;
