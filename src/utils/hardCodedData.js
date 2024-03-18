95dec58e1dee87e3061b7bfc5d746b4dc1cfe13e
Akash <akashbytes@gmail.com>
2024-03-18 21:21:47 +0530

    footer cards data are added ...

--- src/utils/hardCodedData.js
+++ src/utils/hardCodedData.js
@@ -69,3 +69,31 @@ export const footerSecCD=[
         key:"00A4"
     },
 ]
+
+export const acordionData= [
+    {
+        key:"ACC01",
+        hText:"What is Netflix ?",
+        bText:"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price.There's always something new to discover, and new TV shows and movies are added every week!"
+    },
+    {
+        key:"ACC02",
+        hText:"How much dose netflix cost ?",
+        bText:"Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹649 to ₹149 a month. No extra costs, no contracts."
+    },
+    {
+        key:"ACC03",
+        hText:"Where can I watch?",
+        bText:"Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles."
+    },
+    {
+        key:"ACC04",
+        hText:"How do I cancel?",
+        bText:"Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime."
+    },
+    {
+        key:"ACC05",
+        hText:"What can I watch on Netflix?",
+        bText:"Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want. "
+    },
+]
