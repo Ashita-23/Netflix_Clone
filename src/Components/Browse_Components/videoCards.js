24f555f702a36c712f7aa9739612b1028fc9cf2d
Akash <akashbytes@gmail.com>
2024-03-18 20:04:41 +0530

    VideoCards component for movie cards..

--- src/Components/Browse_Components/videoCards.js
+++ src/Components/Browse_Components/videoCards.js
@@ -1,13 +1,12 @@
-// import {POSTER_API} from "../../utils/apiOptions"
+import {POSTER_API} from "../../utils/apiOptions"
 
-const NowPlayingCards =(NowData)=>{
-    console.log(NowData)
-    // if (NowData===null) return
+const NowPlayingCards =({NowData,titleText})=>{
+    
     return(
-        <div className="p-2">
-        <h1 className="text-2xl font-semibold p-2" >Now Playing </h1>
+        <div className="p-2"  >
+       <h1 className="text-2xl font-semibold p-2 text-white" key={titleText.key} >{titleText.titleText} </h1>
          <div className="flex overflow-x-scroll p">
-         {/* {NowData?.map((data)=><img src={POSTER_API+data?.poster_path} alt="poster" className="w-[150px] mx-2"></img>)} */}
+         {NowData?.map((data)=>{return(<img src={POSTER_API+data?.poster_path} alt="poster" className="w-[150px] mx-2" key={NowData.vote_count} ></img>)})}
 
          </div>
         </div>
