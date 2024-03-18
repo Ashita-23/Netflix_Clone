db3656438ff71941220688e76c44b3eac95636d4
Akash <akashbytes@gmail.com>
2024-03-18 21:21:22 +0530

    footer cards are added ..

--- src/Components/Sign_inup_components/Sign_in.js
+++ src/Components/Sign_inup_components/Sign_in.js
@@ -1,13 +1,14 @@
-
+import FooterCards from "./FooterCards"
+import FooterQues from "./FooterQues"
 import netflixCover from "../../Assets/LoginImages/netflixCover.jpeg"
-import { footerCardsData ,footerSecCD } from "../../utils/hardCodedData"
+
 
 const SignIn = () => {
     // console.log(footerCardsData)
     return   (<>
       
-        <nav className="w-[100%]  h-20 px-4 absolute bg-gradient-to-b from-black z-20">
-        <span className="text-red-600 text-center ml-[0.5rem]  font-bold text-[2rem] tracking-wider sm: md:">CHALCHITRA</span></nav> 
+        {/* <nav className="w-[100%]  h-20 px-4 absolute bg-gradient-to-b from-black z-20"> */}
+        {/* <span className="text-red-600 text-center ml-[0.5rem]  font-bold text-[2rem] tracking-wider sm: md:">CHALCHITRA</span></nav>  */}
          <div className="w-[100%] h-[auto]  border border-green-500">
             <img src={netflixCover}  alt="background image" className="w-screen "/>
             <div className="border border-green-600 mt-[-40%] ml-[40%] bg-black  absolute py-6 rounded-md"> 
@@ -27,30 +28,10 @@ const SignIn = () => {
             <div className="text-white px-6 py-0  text-left"><span className="text-sm">If you are already not sign in ? than </span><button className="text-red-500 text-sm">Sign in</button></div>
             </div> 
 
-            <div className="border border-red-700 ">
-                <h1>A plan to suit your needs</h1>
-                <div className="flex">
-                   {footerCardsData?.map((data) =>{return( <div key={data.key}>
-                        <p>{data.title}</p>
-                        <p>{data.text}</p>
-                        <p>{data.cost}</p>
-                    </div>)
-                   })}
-                   
-                </div>
-            </div>
-            <div className="border border-red-700 ">
-                <h1>More reasons to join </h1>
-                <div className="flex">
-                   {footerSecCD?.map((data) =>{return( <div key={data.key}>
-                        <p>{data.title}</p>
-                        <p>{data.text}</p>
-                        <i className={data.Icon}></i>
-                    </div>)
-                   })}
-                   
-                </div>
-            </div>
+          <div className=" bg-black p-6">
+            <FooterCards/>
+            <FooterQues/>
+          </div>
         </div>
        </>)
 }
