867f6a4179918c0de7644119c01f56b834fd6a4a
Akash <akashbytes@gmail.com>
2024-03-18 20:05:50 +0530

    created hard code data for footer cards ....

---
+++ src/utils/hardCodedData.js
@@ -0,0 +1,71 @@
+
+
+
+export const videoCardsTitle=[
+    {   key:"001",
+        titleText:"Now Playing"
+    },
+    {     key:"002",
+        titleText:"Populer "
+    },
+    {     key:"003",
+        titleText:"Top Rated"
+    },
+    {      key:"004",
+        titleText:"Up Coming"
+    },
+]
+
+export const footerCardsData = [
+    {
+        title:"PREMIUM",
+        text:"A cinematic experience with the  best picture and audio quality.",
+        cost:"649/month",
+        key:"00A"
+    },
+    {
+        title:"STANDARD",
+        text:" All the entertainment you love,in Full HD video quality.",
+        cost:"499/month",
+        key:"00B"
+    },
+    {
+        title:"BASIC",
+        text:" A great way to enjoy all your favourite shows and movies on a budget.",
+        cost:"199/month",
+        key:"00C"
+    },
+    {
+        title:"MOBILE",
+        text:"  A travel-friendly option for your mobile devices.",
+        cost:"149/month",
+        key:"00D"
+    },
+]
+
+export const footerSecCD=[
+    {
+        title:"Enjoy on your TV",
+        text:" Watch on smart TVs , PlayStations,Xbos,Chromecast,Apple TV , Blu-ray players and more.",
+        Icon:"fa-solid fa-tv",
+        key:"00A1"
+    },
+    {
+        title:"Download your show to watch offline",
+        text:" Save your favourites easily and always have something to watch.",
+        Icon:"fa-solid fa-arrow-down",
+        key:"00A2"
+    },
+    {
+        title:"watch everywhere ",
+        text:" Stream unlimited movies and TV shows on your phone,tablet,laptop and TV.",
+        Icon:"fa-solid fa-video",
+        key:"00A3"
+    },
+    {
+        title:"Create profiles for kids ",
+        text:" Sent kids on adventures with their favourit characters in a space made just for them-free with your membership.",
+        Icon:"fa-solid fa-user-astronaut",
+        key:"00A4"
+    },
+]
