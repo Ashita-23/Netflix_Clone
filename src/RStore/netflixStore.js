a73d5961ac00ca8f7eafdbc5aa179fff07b273b5
Akash <akashbytes@gmail.com>
2024-03-21 15:36:15 +0530

    added more slices...

--- src/RStore/netflixStore.js
+++ src/RStore/netflixStore.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SearchToggle from "./searchToggle";
 import AccordionToggle from "./accordionToggle"
+import FormToogle from "./formToggle"
+import UserSlice from "./userSlice";
 
 
 const NetFlixStore = configureStore({
     reducer:{
 Search_Toggel : SearchToggle,
-Accordion_Toggle:AccordionToggle
+Accordion_Toggle:AccordionToggle,
+Form_Toogle:FormToogle,
+User_Slice:UserSlice
     }
 })
 
