35e5d653f57e020dc7ece352bf96be9ae0b1e449
Akash <akashbytes@gmail.com>
2024-03-22 16:59:05 +0530

    optimized..

--- src/Components/Browse_Components/Browse.js
+++ src/Components/Browse_Components/Browse.js
@@ -1,75 +1,51 @@
-// import  useNowPlayingApi  from "../../customHooks/useNowPlayingApi"
+
 import { useEffect, useState } from "react"
-// import netflixCover from "../../Assets/LoginImages/netflixCover.jpeg"
-import { TOP_RATED, options , POPULER_API, UP_COMING ,NOW_PLAYING_API } from "../../utils/apiOptions"
+import { useNowPlay , useNowPopuler,useNowTop,useUp_coming,useMovies_video} from "../../customFns/useProjectApis"
 import {videoCardsTitle} from "../../utils/hardCodedData"
 import NowPlayingCards from "./videoCards"
+import { options } from "../../utils/apiOptions"
+
 
 const Browse = ()=>{
 
+    const nowPlayingApi=useNowPlay()
+    const nowPopulerApi=useNowPopuler()
+    const nowTop_RatedApi=useNowTop()
+    const nowUp_ComingApi=useUp_coming()
+    const [TrailerKey,setTrailerKey]=useState()
 
-    const [nowPlaying,setNowPlaying] = useState()
-    const [nowPopuler,setNowPopuler] = useState()
-    const [nowTOP_RATED ,setNowTOP_RATED ] = useState()
-    const [nowUP_COMING ,setUP_COMING ] = useState()
-
-
-    useEffect(()=>{
-        getNOW_PLAYING()
-        getPopuler ()
-        getTOP_RATED ()
-        getUp_Coming ()
-    },[])
-
-    const getNOW_PLAYING = async ()=>{
-        const Data = await fetch(NOW_PLAYING_API , options)
-        const JSON = await Data.json()
-
-        setNowPlaying(JSON) }
-
-
-        const getPopuler = async ()=>{
-            const Data = await fetch(POPULER_API, options)
-            const JSON = await Data.json()
-           
-            setNowPopuler(JSON)
-       
-    }
-
-    const getTOP_RATED = async ()=>{
-        const Data = await fetch(TOP_RATED, options)
-        const JSON = await Data.json()
-      
-        setNowTOP_RATED(JSON)
-   
-}
-
-const getUp_Coming = async ()=>{
-    const Data = await fetch(UP_COMING, options)
-    const JSON = await Data.json()
     
-    setUP_COMING(JSON)
-
+    useEffect(()=>{getData()},[])
+const getData= async()=>{
+    const ApiLink="https://api.themoviedb.org/3/movie/"+nowPlayingApi?.results[0]?.id+"/videos?language=en-US"
+    const Api = await fetch(ApiLink, options)
+    const Json = await Api.json()
+    const FilterTrailer = Json?.results?.filter((data)=> data.type === "Trailer")
+    const Trailer = FilterTrailer.length !==0  ? FilterTrailer[0]:Json?.results[0]
+    setTrailerKey(Trailer.key)
 }
-
+// 
     return(
 <div className="relative w-[100%]">
-    {/* <img alt="Cover" src={netflixCover} className="w-screen"></img> */}
-    <iframe className=" w-screen aspect-video  " src="https://www.youtube.com/embed/xCVBU07uGf4?si=IKSTJwvth33OXRpI?autoplay&mute=1" 
-    title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    <iframe className=" w-screen aspect-video  " src={"https://www.youtube.com/embed/"+TrailerKey+"?&autoplay=1&mute=1" }
+    title="YouTube video player"    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowFullScreen></iframe>
-    <div className=" w-5/12 p-2 absolute -mt-[25%] ml-6 text-white">
-        <p className="text-3xl font-semibold">{nowPlaying?.results[0]?.original_title}</p>
-        <p className="text-md">{nowPlaying?.results[0]?.overview}</p>
-    </div>
-    <div className="border bg-black">
-    <div className="p-2 -mt-[14%]">
-               <NowPlayingCards  NowData={nowPlaying?.results} titleText={videoCardsTitle[0]} key={nowPlaying?.results.id}></NowPlayingCards></div>
-               <NowPlayingCards  NowData={nowPopuler?.results} titleText={videoCardsTitle[1]} key={nowPlaying?.results.id} ></NowPlayingCards>
-               <NowPlayingCards  NowData={nowTOP_RATED?.results} titleText={videoCardsTitle[2]} key={nowPlaying?.results.id}></NowPlayingCards>
-               <NowPlayingCards  NowData={nowUP_COMING?.results} titleText={videoCardsTitle[3]} key={nowPlaying?.results.id}></NowPlayingCards>
+    <div className="w-screen aspect-video absolute bg-gradient-to-r from-black -mt-[55%] -z-[-10]">
+ <div className=" w-5/12 p-2 mt-[25%] ml-6 text-white">
+        <p className="text-3xl font-semibold">{nowPlayingApi?.results[0]?.original_title}</p>
+        <p className="text-md p-1">{nowPlayingApi?.results[0]?.overview}</p>
+        <button className="px-8 py-2 text-md font-medium opacity-75 text-black  bg-white cursor-pointer rounded-sm mr-1 hover:bg-slate-300 "><i className="fa-solid fa-play"></i> Play</button>
+        <button className="px-8 py-2 text-md text-white opacity-75 bg-slate-800 cursor-pointer rounded-sm hover:bg-slate-500 "> <i className="fa-solid fa-circle-info"></i> More Info</button>
+    </div> </div>
+     <div className="border bg-black  ">
+    <div className="p-2 -mt-[14%] z-40 absolute">
+               <NowPlayingCards  NowData={nowPlayingApi?.results} titleText={videoCardsTitle[0]} key={nowPlayingApi?.results.id}></NowPlayingCards></div>
+               <div className=" mt-[7rem]">
+               <NowPlayingCards  NowData={ nowPopulerApi?.results} titleText={videoCardsTitle[1]} key={nowPopulerApi?.results.id} ></NowPlayingCards>
+               <NowPlayingCards  NowData={nowTop_RatedApi?.results} titleText={videoCardsTitle[2]} key={nowTop_RatedApi?.results.id}></NowPlayingCards>
+               <NowPlayingCards  NowData={nowUp_ComingApi?.results} titleText={videoCardsTitle[3]} key={nowUp_ComingApi?.results.id}></NowPlayingCards></div>
 
-    </div>
+    </div> 
 </div>
     )
 }
--- src/Components/Browse_Components/videoCards.js
+++ src/Components/Browse_Components/videoCards.js
@@ -6,7 +6,7 @@ const NowPlayingCards =({NowData,titleText})=>{
         <div className="p-2"  >
        <h1 className="text-2xl font-semibold p-2 text-white" key={titleText.key} >{titleText.titleText} </h1>
          <div className="flex overflow-x-scroll p">
-         {NowData?.map((data)=>{return(<img src={POSTER_API+data?.poster_path} alt="poster" className="w-[150px] mx-2" key={NowData.vote_count} ></img>)})}
+         {NowData?.map((data)=>{return(<img src={POSTER_API+data?.poster_path} alt="poster" className="w-[150px] mx-2" key={NowData?.vote_count} ></img>)})}
 
          </div>
         </div>
